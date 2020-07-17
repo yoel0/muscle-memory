1b6dc52ae353c15801e0717010f30559cd9c21ad
yoel <yoel@macbook-pro.lan>
2020-07-17 16:19:05 -0700

    [fav excercise] added Jai Wolf

--- app.js
+++ app.js
@@ -86,3 +86,103 @@ function doesThisReallyWork(boolean) {
   }
 }
 doesThisReallyWork(doesThisWork);
+
+const sectionOne = document.getElementById("section-one");
+console.log(sectionOne);
+
+const sectionTwo = document.getElementById("section-two");
+console.log(sectionTwo);
+
+const sectionThree = document.querySelector("#section-three");
+console.log(sectionThree);
+
+const sectionFour = document.querySelector("#section-four");
+console.log(sectionFour);
+
+const sectionFive = document.querySelector("#section-five");
+console.log(sectionFive);
+
+// use this henceforth - Prof. Bell
+
+sectionOne.textContent = "Biggie";
+sectionTwo.textContent = "Tupac";
+sectionThree.textContent = "Glazed Doughnuts";
+sectionFour.textContent = "Humble Honey";
+sectionFive.textContent = "Singh is King";
+
+// DOM (most common mistake when working with the DOM is forgetting the . before class)
+
+const paraOne = document.querySelector(".paragraph-one");
+console.log(paraOne);
+
+paraOne.textContent = "It was all a DREAM";
+
+const paraTwo = document.querySelector(".paragraph-two");
+console.log(paraTwo);
+
+paraTwo.textContent = "Hit them UP";
+
+const paraThree = document.querySelector(".paragraph-three");
+console.log(paraThree);
+
+paraThree.textContent = "Krispay kraym";
+
+const paraFour = document.querySelector(".paragraph-four");
+console.log(paraFour);
+
+paraFour.textContent = "bee my bebe";
+
+const paraFive = document.querySelector(".paragraph-five");
+console.log(paraFive);
+
+paraFive.textContent = "Masala is Lyfe";
+
+//
+
+const containerSix = document.createElement("div");
+containerSix.classList.add("container-six");
+console.log(containerSix);
+
+const sectionSix = document.createElement("h2");
+sectionSix.setAttribute("id", "section-six");
+sectionSix.textContent = "Weezy";
+console.log(sectionSix);
+
+const paraSix = document.createElement("p");
+paraSix.classList.add("paragraph-six");
+paraSix.textContent = "Turn your fruits in to Vegtables";
+console.log(paraSix);
+
+const body = document.querySelector("body");
+console.log(body);
+
+body.appendChild(containerSix);
+containerSix.appendChild(sectionSix);
+containerSix.appendChild(paraSix);
+
+// create another container
+const containerSeven = document.createElement("div");
+// add class
+containerSeven.classList.add("container-seven");
+console.log(containerSeven);
+// create another section
+const sectionSeven = document.createElement("h2");
+// add id
+sectionSeven.setAttribute("id", "section-seven");
+// add textContent
+sectionSeven.textContent = "Jai Wolf";
+console.log(sectionSeven);
+// create another paragraph
+const paraSeven = document.createElement("p");
+// add a class
+paraSeven.classList.add("paragraph-seven");
+// add textContent
+paraSeven.textContent = "Stargalaxy Travelling Milky way Bruising";
+console.log(paraSeven);
+
+// append the container to the body
+body.appendChild(containerSeven);
+// append section to the container
+containerSeven.appendChild(sectionSeven);
+// append the paragraph to the container
+containerSeven.appendChild(paraSeven);
