40174d4cd71c7085bb377a890b878d2b8cf0ddb3
yoel <yoel@macbook-pro.lan>
2020-07-17 14:09:33 -0700

    [variables] Add 5 variables

---
+++ app.js
@@ -0,0 +1,27 @@
+// variables of different data-types:
+// string
+// number
+// array
+// boolean
+// object (4 different key value pairs) [string], [number], [array], [boolean]
+
+let sentence = "Hi, My name is CocoScrappeh.";
+console.log(sentence);
+
+let age = 42;
+console.log(age);
+
+const myotorcycles = ['Kawasaki', 'Yamaha', 'Suzuki'];
+console.log(motorcycles);
+
+clet doesThisWork = true;
+console.log(doesThisWork);
+
+const lakers = {
+  headquarters: 'Los Angeles',
+  numberOfPlayers: 11,
+  players: ['LBJ', 'Anthony Davis', 'Danny Green'],
+  areChampions: true
+}
+
+console.log(lakers);
