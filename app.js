6487cc33fcec65259855e84adf6448116a107aed
yoel <yoel@macbook-pro.lan>
2020-07-17 14:53:49 -0700

    [functions] Add 5 functions

--- app.js
+++ app.js
@@ -25,3 +25,64 @@ const lakers = {
 };
 
 console.log(lakers);
+
+// make 10 different functions
+// one of your functions need to pass in an array
+// one of your functions need to pass in a callback
+// ..... string
+// ..... object
+// ..... boolean
+// ..... freestyle
+
+// TEST YOUR CODE!!!
+
+function displayCoolBikes(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let bike = array[i];
+
+    if (bike.length > 7) {
+      result.push(bike);
+    }
+  }
+  return result;
+}
+
+console.log(displayCoolBikes(motorcycles));
+
+function removeFromArray(callback, array) {
+  let result = callback(array)[0];
+  console.log(result);
+}
+
+removeFromArray(displayCoolBikes, motorcycles);
+
+function printSentence(string) {
+  console.log(string);
+}
+
+printSentence(sentence);
+printSentence(name);
+
+function printPlayers(object) {
+  console.log(object.players);
+}
+
+printPlayers(lakers);
+
+function printEachPlayer(object) {
+  object.players.forEach((eachPlayer) => {
+    console.log(eachPlayer);
+  });
+}
+printEachPlayer(lakers);
+
+function doesThisReallyWork(boolean) {
+  if (boolean) {
+    console.log("this works");
+  } else {
+    console.log("this does not work");
+  }
+}
+doesThisReallyWork(doesThisWork);
